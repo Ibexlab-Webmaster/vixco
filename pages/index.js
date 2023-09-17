3a6bf704d58402120e6560cc7588cea73d1c4dbe
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2023-09-17 11:59:04 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -405,7 +405,7 @@ export default function Home(props) {
           </div>
           <div className='flex flex-wrap  gap-5 items-center justify-between max-[450px]:flex-col max-[450px]:gap-5 max-[450px]:mb-[40px]'>
             {
-              pageData.noticeSection.items.length > 0 && pageData.noticeSection.items.map((el) => {
+              pageData.noticeSection.items.length > 0 && pageData.noticeSection.items.reverse().map((el) => {
                 return (
                   <Link
                     key={el.id}
